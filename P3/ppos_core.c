9602144ae0fb2040597974abdf53c90f32032305
Mihael-Scofield <mihaelscofield@gmail.com>
2022-11-23 01:38:06 -0300

    P3 completo

--- P3/ppos_core.c
+++ P3/ppos_core.c
@@ -4,29 +4,18 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <ucontext.h>
+
 #include "ppos_data.h"
 #include "ppos.h"
+#include "queue.h"
 
 #define STACKSIZE 64*1024	/* tamanho de pilha das threads */
 
-int numTask;
-task_t Main, *tarefaAtual;
-
-// Inicializa o sistema operacional; deve ser chamada no inicio do main()
-void ppos_init () {
-    /* desativa o buffer da saida padrao (stdout), usado pela função printf */
-    /* linha necessaria por definicao */
-    setvbuf (stdout, 0, _IONBF, 0) ;
-
-    /* Preparacao de variaveis globais do SO */
-    Main.id = 0;
-    numTask = 1;    
-    tarefaAtual = (task_t *) &(Main);
-
-    return;
-}
+int qntTarefasUsuario, maiorID;
+task_t *filaTarefas, tarefaMain, tarefaDispatcher, *tarefaAtual;
 
 // gerência de tarefas =========================================================
+
 
 // Cria uma nova tarefa. Retorna um ID> 0 ou erro.
 int task_create (task_t *task,			        // descritor da nova tarefa
@@ -51,13 +40,28 @@ int task_create (task_t *task,			        // descritor da nova tarefa
        exit (1);
     }    
 
+
     /* Tratamento dos parametros */
     getcontext( & (task->context) );
     makecontext( & (task->context), (void *) ( * (*start_func) ), 1, arg); // Preparacao do Body da task, como visto em aula
     
     /* Identificacao de Task */
-    task->id = numTask;
-    numTask++;
+    task->id = maiorID;
+    maiorID++;
+    qntTarefasUsuario++;
+    task->status = 0; // Seta tarefa como pronta
+
+    /* Adiciona tarefa a fila de tarefas */
+    if (task->id > 1) { // Adiciona apenas se for "task de usuario"
+        task->prev = NULL;
+        task->next = NULL;
+        queue_append( (queue_t **) &filaTarefas, (queue_t *) task);
+        #ifdef DEBUG
+            printf ("task_create: Numero de tarefas %d na Fila de Prontas.\n",  queue_size ((queue_t*) filaTarefas));
+        #endif        
+    } else { // Nao eh task de usuario, nao precisa ser adicionada a fila
+        qntTarefasUsuario--;
+    }
 
     /* Tratamento de saida */
     #ifdef DEBUG
@@ -68,13 +72,16 @@ int task_create (task_t *task,			        // descritor da nova tarefa
 
 // Termina a tarefa corrente, indicando um valor de status encerramento
 void task_exit (int exit_code) {
-    exit_code++; // APENAS PARA EVITAR WARNING, VARIAVEL SERA USADA NO FUTURO
-
     #ifdef DEBUG
         printf("task_exit: Terminando tarefa: %d. \n", tarefaAtual->id);
     #endif
 
-    task_switch(&(Main));
+    exit_code++; // APENAS PARA EVITAR WARNING
+
+    tarefaAtual->status = -1;
+   
+    qntTarefasUsuario--;
+    task_switch( &(tarefaDispatcher) ); // Agora quem controla a exclusao de tarefas eh o dispatcher
     return;
 }
 
@@ -108,37 +115,130 @@ int task_id () {
     return tarefaAtual->id;
 }
 
+// tarefa que faz o controle de quem sera executada pelo Dispatcher em seguida 
+// por enquanto esta implementada com o padrao FCFS, devido a fila ser ciclica,
+// apenas pegar o proximo a partir do primeiro garante a propriedade de FCFS
+task_t* scheduler () {
+    #ifdef DEBUG
+        printf("scheduler: Scheduler pegando o proximo da fila de tarefas no padrão FCFS. \n");
+    #endif
+    
+    // Pega o proximo da fila
+    filaTarefas = filaTarefas->next;
 
-/*
+    // Retorna o proximo da fila
+    #ifdef DEBUG
+        printf("scheduler: Tarefa selecionada: %d. \n", filaTarefas->id);
+    #endif    
+    return filaTarefas;
+}
 
-função dispatcher
-início
+// Funcao responsavel por matar tarefas,
+// nao eh possivel usar a tarefaAtual pois isso pode gerar perca de ponteiro
+int task_kill (task_t *tarefaMorrendo) {
+    #ifdef DEBUG
+        printf ("task_kill: Matando a tarefa %d. \n", tarefaMorrendo->id);
+    #endif
+
+    // Remove da fila de tarefas
+    if (filaTarefas->prev != NULL) {
+        filaTarefas = filaTarefas->prev; // Retorna para garantir a propriedade FCFS
+    }        
+    int statusRemocao = queue_remove( (queue_t**) &filaTarefas, (queue_t*) tarefaMorrendo);
+    
+    // Libera a memoria da Stack
+    free((tarefaMorrendo->context).uc_stack.ss_sp);
+
+    #ifdef DEBUG
+        if (statusRemocao == 0) {
+            printf ("task_kill: Tarefa %d esta morta. \n", tarefaMorrendo->id);
+        } 
+        else {
+            printf ("task_kill: Erro ao matar a tarefa: %d. \n", tarefaMorrendo->id);
+        }
+    #endif
+
+    return statusRemocao;
+}
+
+// libera o processador para a próxima tarefa, retornando à fila de tarefas
+// prontas ("ready queue")
+void task_yield () {
+    #ifdef DEBUG
+        printf ("task_yield: Yield solicitado pela tarefa %d. \n", tarefaAtual->id);
+    #endif
+
+    task_switch( & (tarefaDispatcher) );
+    return;
+}
+
+void dispatcher_body () {
+    #ifdef DEBUG
+        printf("dispatcher_body: Inicializando o Dispatcher. \n");
+    #endif
+
+    task_t *tarefaAtualUsuario; // Necessario para nao haver perca de ponteiro entre essa e a tarefaAtual global
+    tarefaAtualUsuario = filaTarefas;
+    task_switch(& ( * tarefaAtualUsuario) ); // Apenas para garantir que o looping comece usando a tarefa inicial
+    
+    int auxErro = 0;
+
    // enquanto houverem tarefas de usuário
-   enquanto ( userTasks > 0 )
-
+   // enquanto ( userTasks > 0 )
+   while ( qntTarefasUsuario > 0 ) {
       // escolhe a próxima tarefa a executar
-      próxima = scheduler()
+      // próxima = scheduler()
+      tarefaAtualUsuario = scheduler();
 
       // escalonador escolheu uma tarefa?      
-      se próxima <> NULO então
+      // se próxima <> NULO então
+      if (tarefaAtualUsuario != NULL) {
+        // transfere controle para a próxima tarefa
+        // task_switch (próxima)
+        task_switch(& ( * tarefaAtualUsuario) );
+      }
+                 
+        // voltando ao dispatcher, trata a tarefa de acordo com seu estado
+        //  caso o estado da tarefa "próxima" seja:
+        //      -1  = TERMINADA : ...
+        //       0  = PRONTA    : ...
+        //       2  = SUSPENSA  : ...
+        //     (etc)
+        //  fim caso
+        switch (tarefaAtualUsuario->status) {
+            // TERMINADA
+            case -1: 
+                auxErro = task_kill(tarefaAtualUsuario);
+                if (auxErro == -1) {
+                    fprintf(stderr, "Erro ao matar a Tarefa %d", tarefaAtualUsuario->id);
+                }                
+                break;
+            }
+    }
 
-         // transfere controle para a próxima tarefa
-         task_switch (próxima)
-         
-         // voltando ao dispatcher, trata a tarefa de acordo com seu estado
-         caso o estado da tarefa "próxima" seja:
-            PRONTA    : ...
-            TERMINADA : ...
-            SUSPENSA  : ...
-            (etc)
-         fim caso         
+    // encerra a tarefa dispatcher
+    #ifdef DEBUG
+        printf("dispatcher_body: Encerrando o Dispatcher. \n");
+    #endif    
+   
+   // task_exit(0);
+   task_switch(&tarefaMain); // Garante o retorno para a main, acabando o codigo
+}
 
-      fim se
+// Inicializa o sistema operacional; deve ser chamada no inicio do main()
+void ppos_init () {
+    /* desativa o buffer da saida padrao (stdout), usado pela função printf */
+    /* linha necessaria por definicao */
+    setvbuf (stdout, 0, _IONBF, 0) ;
 
-   fim enquanto
+    /* Preparacao de variaveis globais do SO */
+    tarefaMain.id = 0; 
+    tarefaAtual = (task_t *) &(tarefaMain);
 
-   // encerra a tarefa dispatcher
-   task_exit(0)
-fim
+    maiorID = 1; // Main seria a ID = 0, enquanto o Dispatcher ID = 1
+    qntTarefasUsuario = 0;
 
-*/
+    task_create(&tarefaDispatcher, dispatcher_body, NULL);
+
+    return;
+}
